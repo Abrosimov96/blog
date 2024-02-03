62c638a1bda72eda34088bf0b6321a9f2b0756b6
Abrosimov96 <Abrosimov0423@gmail.com>
2024-02-03 14:26:10 +0200

    add i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {classNames} from 'shared/lib/classNames/classNames';
 import {useTheme} from 'app/providers/ThemeProvider';
 import {AppRouter} from 'app/providers/routes';
 import {Navbar} from 'widgets/Navbar';
 import {Sidebar} from 'widgets/Sidebar';
 
-
 const App = () => {
     const {theme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar/>
-            <div className={'contentPage'}>
-                <Sidebar/>
-                <AppRouter/>
-            </div>
+            <Suspense fallback="">
+                <Navbar/>
+                <div className={'contentPage'}>
+                    <Sidebar/>
+                    <AppRouter/>
+                </div>
+            </Suspense>
         </div>
     );
 };
