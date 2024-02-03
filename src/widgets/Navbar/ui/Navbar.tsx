ed26d5bcb370d1c390946b8a310352c6023568b3
Abrosimov96 <Abrosimov0423@gmail.com>
2024-02-03 12:36:10 +0200

    add shared ui AppLink

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
 import {classNames} from 'shared/lib/classNames/classNames';
 import cl from './Navbar.module.scss'
+import {AppLink, AppLinkTheme} from 'shared/ui/AppLink/AppLink';
 
 interface NavbarProps {
     className?: string;
@@ -9,9 +8,15 @@ interface NavbarProps {
 
 export const Navbar = ({className}: NavbarProps) => {
     return (
-        <div className={classNames(cl.navbar, {}, [className])} >
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
+        <div className={classNames(cl.Navbar, {}, [className])}>
+            <div className={cl.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>
+                    Main
+                </AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
+                    About
+                </AppLink>
+            </div>
         </div>
     );
 };
